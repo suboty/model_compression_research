459355e7eaf64519d8f0803923604ccc6f6a5514
suboty <seed.suboty@gmail.com>
2023-10-31 18:10:28 +0300

    add ViT model

---
+++ model/README.md
@@ -0,0 +1,6 @@
+---
+license: apache-2.0
+metrics:
+- accuracy
+---
+See https://www.kaggle.com/code/dima806/dogs-vs-cats-image-detection-vit for details.
---
+++ original_model/model.py
@@ -0,0 +1,27 @@
+import torch
+from transformers import AutoFeatureExtractor, AutoModelForImageClassification
+
+
+class ViTModel:
+    def __init__(self, path_to_model):
+        self.extractor = AutoFeatureExtractor.from_pretrained(path_to_model)
+        self.model = AutoModelForImageClassification.from_pretrained(path_to_model)
+
+    def predict(self, img):
+        with torch.no_grad():
+            logits = self.model(**img).logits
+
+        predicted_label = logits.argmax(-1).item()
+
+        return self.model.config.id2label[predicted_label]
+
+    def _get_size(self):
+        param_size = 0
+        for param in self.model.parameters():
+            param_size += param.nelement() * param.element_size()
+
+        buffer_size = 0
+        for buffer in self.model.buffers():
+            buffer_size += buffer.nelement() * buffer.element_size()
+
+        return (param_size + buffer_size) / (1024 ** 2)
