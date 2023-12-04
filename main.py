bfdd9ff9dee2004834b7462a9d7742c78ac9f01d
suboty <seed.suboty@gmail.com>
2023-12-04 17:45:15 +0300

    refactor original model code

--- main.py
+++ main.py
@@ -15,8 +15,15 @@ class TestRun:
     def __init__(self,
                  path_to_images: Path):
         self.path_to_images = path_to_images
-        self.images_list = os.listdir(path_to_images)
+        self.images_list = [x for x in os.listdir(path_to_images) if x != '.gitkeep']
         self.models = []
+
+    @staticmethod
+    def _count_accuracy(target_list, predict_list):
+        if len(target_list) != len(predict_list):
+            raise AttributeError(f'Error with equality lengths. '
+                                 f'Target len: {len(target_list)}.'
+                                 f'Predict len: {len(predict_list)}')
 
     def run(self):
         start_time = time.time()
@@ -26,33 +33,41 @@ class TestRun:
 
         for model in self.models:
 
-            for image_name in self.images_list:
+            logger.info(f'Model {model[0]}')
 
-                image = Image.open(Path(self.path_to_images, image_name), mode='r', formats=None)
+            try:
 
-                inputs = model.extractor(image, return_tensors="pt")
-                predict = model.predict(inputs)
-                target = image_name[:image_name.find(".")]
+                for image_name in self.images_list:
 
-                if target == "dog":
-                    label = 1
-                else:
-                    label = 0
+                    image = Image.open(Path(self.path_to_images, image_name), mode='r', formats=None)
+                    predict = model[1].predict(_model=model[1], img=image)
+                    target = image_name[:image_name.find(".")]
 
-                target_list.append(label)
+                    if target == "dog":
+                        label = 1
+                    else:
+                        label = 0
 
-                if predict == "dogs":
-                    pr = 1
-                else:
-                    pr = 0
+                    target_list.append(label)
 
-                predict_list.append(pr)
+                    if predict == "dogs":
+                        pr = 1
+                    else:
+                        pr = 0
 
-            end_time = time.time()
+                    predict_list.append(pr)
 
-            logger.info("Время обработки изображений исходной модели= ", end_time - start_time, " секунд")
-            logger.info("Скорость обработки изображений у исходной модели составила  ",
-                        len(self.images_list) / (end_time - start_time), " изображений в секунду")
+                end_time = time.time()
+
+                logger.info(f"--- Time for model processing = {end_time - start_time} seconds")
+                logger.info(f"--- Speed of model processing is "
+                            f"{len(self.images_list) / (end_time - start_time)} images per second")
+
+                logger.info(f'--- Model {model[0]} Accuracy is: '
+                            f'{self._count_accuracy(target_list=target_list, predict_list=predict_list)}')
+
+            except Exception as e:
+                logger.critical(f'--- Error with model {model[0]}. Error: {e}')
 
 
 if __name__ == '__main__':
@@ -63,9 +78,9 @@ if __name__ == '__main__':
     onnx_model = ONNXViTModel(path_to_model=Path('model'))
     tensorrt_model = TensorRTViTModel(path_to_model=Path('model'))
 
-    test_compression_run.models.append(original_model)
-    test_compression_run.models.append(openvino_model)
-    test_compression_run.models.append(onnx_model)
-    test_compression_run.models.append(tensorrt_model)
+    test_compression_run.models.append(('original_model', original_model))
+    test_compression_run.models.append(('openvino_model', openvino_model))
+    test_compression_run.models.append(('onnx_model', onnx_model))
+    test_compression_run.models.append(('tensorrt_model', tensorrt_model))
 
     test_compression_run.run()
--- original_model/model.py
+++ original_model/model.py
@@ -7,9 +7,10 @@ class ViTModel:
         self.extractor = AutoFeatureExtractor.from_pretrained(path_to_model)
         self.model = AutoModelForImageClassification.from_pretrained(path_to_model)
 
-    def predict(self, img):
+    def predict(self, _model, img):
+        inputs = _model.extractor(img, return_tensors="pt")
         with torch.no_grad():
-            logits = self.model(**img).logits
+            logits = self.model(**inputs).logits
 
         predicted_label = logits.argmax(-1).item()
 
